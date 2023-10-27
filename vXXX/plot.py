25ffb1f89298ef4a109258b7c7c8c82933c89917
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2023-10-27 12:36:02 +0200

    alles iwi und plot

--- vXXX/plot.py
+++ vXXX/plot.py
@@ -5,7 +5,7 @@ x = np.linspace(0, 10, 1000)
 y = x ** np.sin(x)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
+ax1.plot(x, y, label="Test Kurve")
 ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
 ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
 ax1.legend(loc="best")
