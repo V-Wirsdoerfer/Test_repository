5d9ec2e5723d4def8b88b00ae04010c158992507
Vincent Wirsdörfer <vincent.wirsdoerfer@udo.edu>
2025-04-30 14:59:46 +0200

    elektronik blatt 5 aufgabe 9 Bode Plot hinzugefügt

---
+++ elek_A9/plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+f, U_out, delta_t = np.genfromtxt("data.txt", unpack=True)
+
+U_in = 10
+
+L_U=20 * np.log10(U_out/U_in)
+
+delta_phi=2*np.pi*f*delta_t
+
+#plot
+fig, ax1 = plt.subplots(layout="constrained")
+
+ax1.plot(f,L_U, color="blue", label="Spannungspegel")
+ax2=ax1.twinx()
+ax2.plot(f, delta_phi,color="green" , label="Phasenverschiebung")
+
+ax2.legend()
+ax1.grid()
+ax1.set(
+    xscale="log",
+    xlabel="Frequenz / Hz",
+    ylabel=r"$L_U$ /dB",
+)
+ax2.set(
+    ylabel="Phasenverschiebung / rad",
+)
+ax1.legend()
+
+
+
+fig.savefig("build/Bode.pdf")
--- vXXX/plot.py
+++ vXXX/plot.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
-
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="ganze halbe Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
-
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
-
-fig.savefig("build/plot.pdf")
+#x = np.linspace(0, 10, 1000)
+#y = x ** np.sin(x)
+#
+#fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
+#ax1.plot(x, y, label="ganze halbe Kurve")
+#ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+#ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+#ax1.legend(loc="best")
+#
+#ax2.plot(x, y, label="Kurve")
+#ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+#ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+#ax2.legend(loc="best")
+#
+#fig.savefig("build/plot.pdf")
+#
+print("Eingabe: ")
+testin = input()
+print("Du hast geschrieben:", testin)
 
 # Test commit
